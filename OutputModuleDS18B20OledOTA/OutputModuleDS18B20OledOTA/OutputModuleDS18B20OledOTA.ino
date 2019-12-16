d449b25caa4023c403f0b044516d2fe5d95906f9
mirsmok <mirmsok@wp.pl>
2019-12-16 21:19:01 +0100

    add OTA for output module

--- OutputModuleDS18B20OledOTA/OutputModuleDS18B20OledOTA/OutputModuleDS18B20OledOTA.ino
+++ OutputModuleDS18B20OledOTA/OutputModuleDS18B20OledOTA/OutputModuleDS18B20OledOTA.ino
@@ -32,8 +32,27 @@ CONSEQUENTIAL DAMAGES(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE G
  Author:  gmag11@gmail.com
  Editor:  http://www.visualmicro.com
 */
+
+//#define NTP_ON
+//#define OLED_ON
+
+/************ Pin definition for use generic module***************/
+
+static const uint8_t D0 = 16;
+static const uint8_t D1 = 5;
+static const uint8_t D2 = 4;
+static const uint8_t D3 = 0;
+static const uint8_t D4 = 2;
+static const uint8_t D5 = 14;
+static const uint8_t D6 = 12;
+static const uint8_t D7 = 13;
+static const uint8_t D8 = 15;
+static const uint8_t RX = 3;
+static const uint8_t TX = 1;
+
 // Include the correct display library
 // For a connection via I2C using Wire include
+#ifdef OLED_ON
 #include <Wire.h>   // Only needed for Arduino 1.6.5 and earlier
                     //#include "SSD1306.h" // alias for `#include "SSD1306Wire.h"`
 #include "SH1106.h" // alis for `#include "SH1106Wire.h"`
@@ -72,18 +91,22 @@ typedef void (*Demo)(void);
 
 int demoMode = 0;
 int counter = 1;
-char aktualnyCzas[100];
+//char aktualnyCzas[100];
 
 /*oled interface */
-
+#endif
 #include <TimeLib.h>
 //#include "WifiConfig.h"
 #include "images.h"
-#include <NtpClientLib.h>
 #include <ESP8266WiFi.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#ifdef NTP_ON
+#include <NtpClientLib.h>
+#endif
 #ifndef WIFI_CONFIG_H
 #define YOUR_WIFI_SSID "NETIASPOT-C61350"
 #define YOUR_WIFI_PASSWD "wiktorpatryk"
@@ -94,22 +117,25 @@ OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature DS18B20(&oneWire);
 float actualTemperature;
 String setpointTemperature, roomTemperature;
+int noResponseCounter;
 
 #define ONBOARDLED 2 // Built in LED on ESP-12/ESP-07
 #define OUTPUT_PIN D0
-bool outputState;
+volatile bool outputState;
 
 WiFiClient client;
-bool clientConnected, dataExchangeOk;
-const char *host = "192.168.1.201";
+bool clientConnected, dataExchangeOk,clientCleared;
+const char *host = "192.168.1.27";
 int8_t timeZone = 1;
 
 // Start NTP only after IP network is connected
 void onSTAGotIP(WiFiEventStationModeGotIP ipInfo)
 {
   Serial.printf("Got IP: %s\r\n", ipInfo.ip.toString().c_str());
+#ifdef NTP_ON
   NTP.begin("pool.ntp.org", timeZone, true);
   NTP.setInterval(63);
+#endif
   digitalWrite(ONBOARDLED, LOW); // Turn on LED
 }
 
@@ -122,6 +148,7 @@ void onSTADisconnected(WiFiEventStationModeDisconnected event_info)
   //NTP.stop(); // NTP sync can be disabled to avoid sync errors
 }
 
+#ifdef NTP_ON
 void processSyncEvent(NTPSyncEvent_t ntpEvent)
 {
   if (ntpEvent)
@@ -141,14 +168,17 @@ void processSyncEvent(NTPSyncEvent_t ntpEvent)
 
 boolean syncEventTriggered = false; // True if a time even has been triggered
 NTPSyncEvent_t ntpEvent;            // Last triggered event
-
+#endif
+#ifdef OLED_ON
 void drawTime()
 {
   // Font Demo1
   // create more fonts at http://oleddisplay.squix.ch/
   display.setTextAlignment(TEXT_ALIGN_LEFT);
   display.setFont(ArialMT_Plain_10);
+#ifdef NTP_ON
   display.drawString(86, 0, aktualnyCzas);
+#endif
   display.drawString(0, 0, "WODA: " + String(actualTemperature));
   display.drawString(75, 51, "AKT: " + roomTemperature);
   display.drawString(0, 51, "SET: " + setpointTemperature);
@@ -161,6 +191,7 @@ void drawTime()
 Demo demos[] = {drawTime}; //, drawTextFlowDemo, drawTextAlignmentDemo, drawRectDemo, drawCircleDemo, drawProgressBarDemo, drawImageDemo};
 int demoLength = (sizeof(demos) / sizeof(Demo));
 long timeSinceLastModeSwitch = 0;
+#endif
 //output module functions
 bool connectToServer(void)
 {
@@ -169,7 +200,8 @@ bool connectToServer(void)
   {
     if (client.connect(host, 1500))
     {
-      Serial.println("client connected");
+      Serial.println("client connected"); 
+      clientCleared=false;
       return true;
     }
     else
@@ -203,10 +235,19 @@ String findTag(String input, String tag)
   }
 }
 
+void sendStr(String str)
+{  
+  if (client.connected()){
+    client.println(str);
+  }
+}
+
 void checkClientResponse(void)
 {
+  //<content><setpointTemperature>21.0</setpointTemperature><roomTemperature>22.0</roomTemperature><outputState>ON</outputState></content>
+  //<content><setpointTemperature>21.0</setpointTemperature><roomTemperature>22.0</roomTemperature><outputState>OFF</outputState></content>
+  
   static int counter = 0;
-  static int noResponseCounter;
   String value;
   while (client.available())
   {
@@ -217,6 +258,8 @@ void checkClientResponse(void)
     if (!findTag(line, "roomTemperature").equals(""))
       roomTemperature = findTag(line, "roomTemperature");
     value = findTag(line, "outputState");
+    String str="Otrzymano: ";
+    str+=value;
     if (!value.equals(""))
     {
       if (value.equals("ON"))
@@ -230,14 +273,14 @@ void checkClientResponse(void)
     }
     noResponseCounter = 0;
   }
-  if (actualTemperature < 1.0 || actualTemperature > 70.0)
-    outputState = false;
   if (noResponseCounter > 2)
   {
+    clientCleared=true;
     outputState = false;
     dataExchangeOk = false;
     client.flush();
     client.stop();
+    noResponseCounter = 0;
   }
   else
   {
@@ -246,34 +289,37 @@ void checkClientResponse(void)
   //delay(100);
   counter += 1;
   if (counter > 30)
-  {
-    sendStatus();
+  {   
     counter = 0;
-    if (noResponseCounter < 20)
-      noResponseCounter++;
   }
 }
 
 void sendStatus(void)
 {
-  String dataToSend = "<content><RSSI>" + String(WiFi.RSSI()) + "</RSSI>";
-  dataToSend += "<dev_type>outputModule</dev_type>";
-  dataToSend += "<id>1002</id>";
-  dataToSend += String("<outputState>") + String(outputState ? "ON" : "OFF") + String("</outputState>");
-  dataToSend += String("<sensorTemperature>") + String(actualTemperature) + String("</sensorTemperature></content>");
-  Serial.println(dataToSend);
-  client.println(dataToSend);
+  
+  if (client.connected()){
+    String dataToSend = "<content><RSSI>" + String(WiFi.RSSI()) + "</RSSI>";
+    dataToSend += "<dev_type>outputModule</dev_type>";
+    dataToSend += "<id>1002</id>";
+    dataToSend += String("<outputState>") + String(outputState ? "ON" : "OFF") + String("</outputState>");
+    dataToSend += String("<sensorTemperature>") + String(actualTemperature) + String("</sensorTemperature></content>");
+    Serial.println(dataToSend);
+    client.println(dataToSend);
+    if (noResponseCounter < 20)
+        noResponseCounter++;
+  }
 }
 
 void setup()
 {
   pinMode(OUTPUT_PIN, OUTPUT);
   // Initialising the UI will init the display too.
+#ifdef OLED_ON
   display.init();
 
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_10);
-
+#endif
   static WiFiEventHandler e1, e2;
 
   Serial.begin(115200);
@@ -284,11 +330,12 @@ void setup()
   pinMode(ONBOARDLED, OUTPUT);    // Onboard LED
   digitalWrite(ONBOARDLED, HIGH); // Switch off LED
 
+#ifdef NTP_ON
   NTP.onNTPSyncEvent([](NTPSyncEvent_t event) {
     ntpEvent = event;
     syncEventTriggered = true;
   });
-
+#endif
   // Deprecated
   /*WiFi.onEvent([](WiFiEvent_t e) {
     Serial.printf("Event wifi -----> %d\n", e);
@@ -296,20 +343,50 @@ void setup()
 
   e1 = WiFi.onStationModeGotIP(onSTAGotIP); // As soon WiFi is connected, start NTP Client
   e2 = WiFi.onStationModeDisconnected(onSTADisconnected);
+
+  /************* OTA configuration ***************************/
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR)
+      Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR)
+      Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR)
+      Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR)
+      Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR)
+      Serial.println("End Failed");
+  });
+  ArduinoOTA.setHostname("outputModule1002");
+  ArduinoOTA.begin();
 }
 
 void loop()
 {
   static int i = 0;
-  static int last = 0;
+  static int last=0,sendTimer = 0,connectTimer = 0;
   static int lastTime = 0;
+  ESP.wdtEnable(10000);
+  ArduinoOTA.handle();
 
+#ifdef NTP_ON
   if (syncEventTriggered)
   {
     processSyncEvent(ntpEvent);
     syncEventTriggered = false;
   }
-
+#endif
+#ifdef OLED_ON
   display.clear();
   // draw the current demo method
   demos[demoMode]();
@@ -322,7 +399,7 @@ void loop()
     demoMode = (demoMode + 1) % demoLength;
     timeSinceLastModeSwitch = millis();
   }
-
+#endif
   if ((millis() - last) > 500)
   {
     //Serial.println(millis() - last);
@@ -335,7 +412,9 @@ void loop()
     Serial.print("Uptime: ");
     Serial.print(NTP.getUptimeString()); Serial.print(" since ");
    Serial.println(NTP.getTimeDateString(NTP.getFirstSync()).c_str());*/
+#ifdef NTP_ON
     strncpy(aktualnyCzas, NTP.getTimeDateString().c_str(), 9);
+#endif
     if (connectToServer())
     {
       clientConnected = true;
@@ -350,9 +429,10 @@ void loop()
     digitalWrite(OUTPUT_PIN, outputState);
   else
   {
-    digitalWrite(OUTPUT_PIN, false);
+    digitalWrite(OUTPUT_PIN, LOW);
     outputState = false;
   }
+  /** wylaczony pomiar temperatury
   if ((millis() - lastTime) > 5000)
   {
     lastTime = millis();
@@ -362,6 +442,14 @@ void loop()
       actualTemperature = DS18B20.getTempCByIndex(0);
     } while ((actualTemperature == 85.0 || actualTemperature == (-127.0)) && ((millis() - lastTime) < 3000));
   }
-
+*/
+  
+  if ((millis() - sendTimer) > 10000)
+  {
+    sendTimer = millis();
+      sendStatus();
+     // digitalRead(OUTPUT_PIN) ? digitalWrite(OUTPUT_PIN,LOW) : digitalWrite(OUTPUT_PIN,HIGH);
+  }
   delay(0);
+  ESP.wdtFeed();
 }
